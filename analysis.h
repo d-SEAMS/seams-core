785952fb99031e3bd5d23ea207608bea01545d5b
Amrita Goswami <amrita16thaug646@gmail.com>
2018-08-23 17:17:06 +0530

    analysis: Init

---
+++ analysis.h
@@ -0,0 +1,24 @@
+#ifndef _ANALYSIS_H
+#define _ANALYSIS_H
+
+#include <fstream>
+#include <math.h>
+#include <string>
+#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "molecule.h"
+#include "parameter.h"
+#include "molecular_system.h"
+
+using namespace std;
+
+class CAnalysis {
+	private:
+		//
+	public:
+		//
+};
+
+#endif
