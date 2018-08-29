bd051f4915090e70bd56c0a13de2e243f8ecf02b
Amrita Goswami <amrita16thaug646@gmail.com>
2018-08-29 18:22:44 +0530

    Output: Basic init. TODO: Make CAnalysis inherit output class

---
+++ output.h
@@ -0,0 +1,20 @@
+#ifndef _OUTPUT_H
+#define _OUTPUT_H
+
+#include <fstream>
+#include <math.h>
+#include <string>
+#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "molecule.h"
+#include "parameter.h"
+
+using namespace std;
+
+class COutput: private COutput {
+
+};
+
+#endif
