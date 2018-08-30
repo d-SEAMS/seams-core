823a3cd26602fd13d33f6e568e3fd0aa7b8da4d8
Amrita Goswami <amrita16thaug646@gmail.com>
2018-08-30 16:58:50 +0530

    Output: Temporarily shifted private function createOutputDir to public, for testing it in main. TODO: Set this as a private function later. Also removed dependence on (buggy?) filesystem header

--- output.h
+++ output.h
@@ -7,18 +7,22 @@
 #include <sstream>
 #include <stdio.h>
 #include <stdlib.h>
-#include <experimental/filesystem> // Remove experimental?
+#include <sys/types.h>
+#include <sys/stat.h>
 
 using namespace std;
 
 class COutput {
   private:
-    //The position of the particle
-    void createOutputDir();
+    
   public:
     COutput();
     virtual ~COutput();
- 	// Print arrays to file
+ 	// Print arrays to file, with the size, and two x,y arrays 
+ 	// as arguments
+ 	void printToFile(int, double*, double* );
+ 	//Checks whether the output directory exists, and creates it if it doesn't
+    void createOutputDir(const char *path);
 };
 
 #endif
