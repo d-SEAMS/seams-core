3377b3caebdf7d7c8f42dbbd1d49ff7e490cbf69
Amrita Goswami <amrita16thaug646@gmail.com>
2018-08-30 12:08:17 +0530

    Output: Added function for creating output directory

--- output.h
+++ output.h
@@ -1,20 +1,24 @@
-#ifndef _OUTPUT_H
+#ifndef _OUTPUT_H     
 #define _OUTPUT_H
 
+#include <iostream>
 #include <fstream>
-#include <math.h>
 #include <string>
 #include <sstream>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include "molecule.h"
-#include "parameter.h"
+#include <experimental/filesystem> // Remove experimental?
 
 using namespace std;
 
-class COutput: private COutput {
-
+class COutput {
+  private:
+    //The position of the particle
+    void createOutputDir();
+  public:
+    COutput();
+    virtual ~COutput();
+ 	// Print arrays to file
 };
 
 #endif
