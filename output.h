bcccbd3606b848720eabbd7ad51a413f41e887fc
Amrita Goswami <amrita16thaug646@gmail.com>
2018-09-01 13:04:42 +0530

    Output: changed function for output folder to private

--- output.h
+++ output.h
@@ -14,15 +14,14 @@ using namespace std;
 
 class COutput {
   private:
-    
+    //Checks whether the output directory exists, and creates it if it doesn't
+    void createOutputDir(const char *path);
   public:
     COutput();
     virtual ~COutput();
  	// Print arrays to file, with the size, and two x,y arrays 
  	// as arguments
- 	void printToFile(int, double*, double* );
- 	//Checks whether the output directory exists, and creates it if it doesn't
-    void createOutputDir(const char *path);
+ 	void printToFile(int, double* x, double* y, const char *filename );
 };
 
 #endif
