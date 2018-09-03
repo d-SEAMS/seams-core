4df4e0386380133b82a5722d9c4e769018601afd
Amrita Goswami <amrita16thaug646@gmail.com>
2018-09-03 10:18:33 +0530

    Output: Added default argument for filename

--- output.h
+++ output.h
@@ -15,13 +15,14 @@ using namespace std;
 class COutput {
   private:
     //Checks whether the output directory exists, and creates it if it doesn't
+    // The directory cannot be created for Windows
     void createOutputDir(const char *path);
   public:
     COutput();
     virtual ~COutput();
  	// Print arrays to file, with the size, and two x,y arrays 
  	// as arguments
- 	void printToFile(int, double* x, double* y, const char *filename );
+ 	void printToFile(int, double* x, double* y, const string& filename = "output");
 };
 
 #endif
