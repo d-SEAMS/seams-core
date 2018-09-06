b8c03e69e522e2cf4628463e40b10163194e94db
Amrita Goswami <amrita16thaug646@gmail.com>
2018-09-06 18:03:46 +0530

    documentaion: Updates

--- html/search/functions_5.js
+++ html/search/functions_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['normalizerdf2d',['normalizeRDF2D',['../classRdf2D.html#acf73dc86f20e82799157ff53994a2754',1,'Rdf2D']]],
+  ['normalizerdf2d',['normalizeRDF2D',['../classRdf2D.html#a9b929a0187119b7300ed9fe4ba41f979',1,'Rdf2D']]],
   ['normalizerdf3d',['normalizeRDF3D',['../classRdf3D.html#a08d573ef51fd88bd42456a76fc3d7ca4',1,'Rdf3D']]]
 ];
