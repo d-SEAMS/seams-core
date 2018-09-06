a277740f1814929fd232fad7b2f3163779652384
Amrita Goswami <amrita16thaug646@gmail.com>
2018-09-06 19:10:14 +0530

    Documentation: Updated README

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # structureFactor
-C++ code that can calculate the structure factor from g(r)
+This C++ program reads in lammps trajectory files
+or XYZ files to calculate RDF, in-plane RDF and the structure factor.
+
+\brief Post-processing code for RDF, in-plane RDF, structure factor
 
 # Compilation
 
@@ -15,4 +18,14 @@ An executable *runme* will be formed in the base directory.
 
 ```bash
 ./runme
-```
+``` 
+
+# Details
+
+* For example, if you want to find the in-plane RDF in the
+XY plane, you should use the Rdf2D class. The equation used for 2D-RDF for the \f$n^{th}\f$ layer is:
+\f[
+ g^n(r) = \frac{1}{(\rho^n)^2 A \delta z} \Sigma_{i \neq j} \delta(r - r_{ij}) \left[ \Theta\left( \frac{\delta z}{2}-|z_i-z^2| \right) \times \Theta\left( \frac{\delta z}{2}-|z_j-z^n| \right) \right] 
+\f]
+  For detailed instructions, see the Rdf2D class documentation
+ 
--- rdf2D.h
+++ rdf2D.h
@@ -21,12 +21,27 @@
  XY plane, you should use the  initRDFxy() function, followed by
  either the accumulateRDFxy() or singleRDFxy(), depending on whether
  you want to average over several frames or not. If you use the accumulateRDFxy
- function, you must also use the normalizeRDF2D() function.
+ function, you must also use the normalizeRDF2D() function. Don't call 
+ call normalizeRDF2D() when using singleRDFxy(), since this function
+ is called by singleRDFxy() itself. The printRDF2D() function prints
+ the radial values and corresponding RDF values to a file called rdf2D.dat
+ in the output folder. 
 
- The equation used for 2D-RDF is the following for the \f$n^{th}\f$ :
+ The equation used for 2D-RDF for the \f$n^{th}\f$ layer is:
   \f[
-  g^n(r) = \frac{1}{(\rho^n)^2 A \delta z} \Sigma_{i \neq j} \delta(r - r_{ij}) \left[ \Theta\left( \frac{\delta z}{2}-|z_i-z^2| \right) \right] \times \Theta\left( \frac{\delta z}{2}-|z_j-z^n| \right)
+  g^n(r) = \frac{1}{(\rho^n)^2 A \delta z} \Sigma_{i \neq j} \delta(r - r_{ij}) \left[ \Theta\left( \frac{\delta z}{2}-|z_i-z^2| \right) \times \Theta\left( \frac{\delta z}{2}-|z_j-z^n| \right) \right] 
   \f]
+  - \f$z^n\f$ is the z coordinate of the layer chosen. This can be determined
+  by finding the peak in the density(or number) vs \f$z\f$ plot.
+  - \f$\delta z\f$ is the thickness of the layer chosen. This can also be determined
+  from the density(or number) vs \f$z\f$ plot by ascertaining the width of the peak corresponding to the desired
+  layer. 
+  - Here, \f$\rho\f$ corresponds to the bulk number density of the species chosen. This is determined in the code
+  by calculating the number of atoms and the volume. By default, the program accepts the simulation box volume
+  as the volume. The user should enter the volume for more complex systems. The accuracy of the RDF is sensitive to 
+  the calculated value of density, so care should be taken when specifying/calculating the density. 
+  - This calculation is computed upto a maximum radius, which can either be user-defined or is set by default to be half 
+  the smallest box length of the \f$x\f$ or \f$y\f$ dimensions.
  */
 
 class Rdf2D: public COutput {
