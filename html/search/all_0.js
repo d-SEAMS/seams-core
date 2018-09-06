e8a3a3d2f11443333c7bb49e1f1f6f7f050e0d32
Amrita Goswami <amrita16thaug646@gmail.com>
2018-09-06 12:51:44 +0530

    Documentaion: Update. TODO: Add equations

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['accumulaterdf3d',['accumulateRDF3D',['../classRdf3D.html#aca3e0b2da042856d705b4bca8f6be335',1,'Rdf3D']]],
-  ['accumulaterdfxy',['accumulateRDFxy',['../classRdf2D.html#ab4c0b7a104e7ee1c3f3044410a21ebfe',1,'Rdf2D']]]
+  ['accumulaterdfxy',['accumulateRDFxy',['../classRdf2D.html#a6c716851d80fd2a7dcfefd219892d87b',1,'Rdf2D']]]
 ];
--- html/search/functions_8.js
+++ html/search/functions_8.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['set_5fposition',['set_position',['../classCMolecule.html#ad9a66ae676bd9ed1012f0c6e952369e0',1,'CMolecule']]],
   ['singlerdf3d',['singleRDF3D',['../classRdf3D.html#a27efc547d859576b41a16d98cd2d1df9',1,'Rdf3D']]],
-  ['singlerdfxy',['singleRDFxy',['../classRdf2D.html#a984b46264ea53aa92f7134509b5da9d0',1,'Rdf2D']]]
+  ['singlerdfxy',['singleRDFxy',['../classRdf2D.html#afc5ff73aa9c126184e94ee3abfc14ff4',1,'Rdf2D']]]
 ];
