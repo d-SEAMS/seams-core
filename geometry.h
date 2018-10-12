19bebe9160f65a3d1c4a88d99255bd5d830870f3
Amrita Goswami <amrita16thaug646@gmail.com>
2018-10-12 18:41:45 +0530

    Geom: Separate class for volume calculations

---
+++ geometry.h
@@ -0,0 +1,49 @@
+#ifndef _GEOMETRY_H
+#define _GEOMETRY_H
+
+#include <fstream>
+#include <math.h>
+#include <string>
+#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "molecule.h"
+#include "parameter.h"
+#include "molecular_system.h"
+#include "output.h"
+
+class CLayer {
+	public:
+		CLayer();
+		virtual ~CLayer();
+
+		// Check whether the given coordinate (r) is within the layer (midpoint r_layer)
+		bool atomInsideLayer(double r, double r_layer, double dr);
+
+};
+
+// Class for volume 
+class CVolume {
+	public:
+		CVolume();
+		virtual ~CVolume();
+
+		// Volume limits (may or may not be set)
+		double xlo, xhi, ylo, yhi, zlo, zhi;
+		// This is true if the volume has not been defined by the user (default)
+		bool notset;
+		// Flag to check if the atom is inside the given volume or not 
+		int volFlag;
+
+		// Check whether the atom is inside the user-defined volume
+		bool atomInsideVol(class CMolecularSystem& molSys, int iatom, double xlo = 0.0, double xhi = 0.0, double ylo=0.0, double yhi=0.0, double zlo=0.0, double zhi=0.0);
+
+		// Check the volume limits
+		void checkVolume();
+		// Check if the atom is within a particular dimension range
+		void atomCoordLimits(double r_atom, double r_min, double r_max);
+};
+
+
+#endif
