d121394846a5e366946f3534350467d6ad39db82
Amrita Goswami <amrita16thaug646@gmail.com>
2018-11-05 20:05:51 +0530

    Density: init of functions to calculate the number density in one dimension

--- density.cpp
+++ density.cpp
@@ -5,11 +5,59 @@
  ***********************************************/
 Density::Density()
 {
-	// 
+	this->binwidth = -1.0;
+	this->typeI = -1;
+	this->number = NULL; 
+	this->coord = NULL; 
 }
 /********************************************//**
  *  Destructor
  ***********************************************/
 Density::~Density()
 {
+	delete [] number;
+	delete [] coord;
+}
+
+
+/********************************************//**
+ *  Initializes the number density. The binning dimension 
+ is the z dimension
+ ***********************************************/
+Density::initNumberZ(class CMolecularSystem& molSys,double binwidth, int typeI, double xlo, double xhi, double ylo, double yhi, double zlo, double zhi)
+{
+	double z_length = zhi-zlo; // Length for binning
+	// If the user has not entered z dimension lengths, the box length is taken as default
+	if (z_length==0.0){z_length=molSys.parameter->boxz;}
+
+	// Assign values
+	this->binwidth = binwidth;
+	this->typeI = typeI;
+	this->xlo=xlo; this->xhi = xhi; 
+	this->ylo=ylo; this->yhi=yhi;
+	this->zlo=zhi; this->zhi;
+
+
+}
+
+
+/********************************************//**
+ *  Gets the number density of a particle of type I
+ in a user-defined volume. If the volume is not defined, the entire 
+ box volume is taken. Binning is done in the z dimension
+ ***********************************************/
+Density::NumberZ(class CMolecularSystem& molSys,double binwidth int typeI,double xlo,double xhi,double ylo,double yhi,double zlo,double zhi)
+{
+	//
+}
+
+
+/********************************************//**
+ *  Gets the number of bins in the binning dimension.
+ Takes maximum length in which binning will be done, and the CMolecularSystem object
+ as the arguments
+ ***********************************************/
+Density::getBins(class CMolecularSystem& molSys, double max_length)
+{
+	//
 }
--- density.h
+++ density.h
@@ -16,7 +16,18 @@
 
 class Density: public COutput, private CVolume {
 	private:
-		// 
+		// No. of snapshots for density binning 
+		int nframes;		
+    	// No. of bins 
+		int nbin;
+		// User-specified binwidth
+    	double binwidth;
+
+		// Initializes the arrays etc
+		void initNumberZ(class CMolecularSystem& molSys,double binwidth, int typeI, double xlo, double xhi, double ylo, double yhi, double zlo, double zhi);
+
+		// Calculate the number of bins given the length in which binning is to be done
+    	void getBins(class CMolecularSystem& molSys, double );
 
 	public:
 		Density();
@@ -25,6 +36,12 @@ class Density: public COutput, private CVolume {
 		// Dynamically allocated array for number distribution and binned coordinate
 		double* number;
 		double* coord;
+
+		// Lammps trajectory IDs of the atom
+		int typeI;
+
+		// Gets the number density of type I in the defined volume limits; binning done in the z dimension
+		void NumberZ(class CMolecularSystem& molSys, double binwidth, int typeI=-1, double xlo = 0.0, double xhi = 0.0, double ylo=0.0, double yhi=0.0, double zlo=0.0, double zhi=0.0);
 };
 
 #endif
