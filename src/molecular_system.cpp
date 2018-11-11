739371341912ef7d06f22656569a005687967e8f
HaoZeke <rohit.goswami@aol.com>
2018-11-12 02:06:57 +0530

    molSys: Speed up traversal

--- src/molecular_system.cpp
+++ src/molecular_system.cpp
@@ -344,6 +344,9 @@ void CMolecularSystem::readParticleFile(int step)
           this->molecules[iatom].molID = molID;
           this->molecules[iatom].atomID = atomID;
 
+          // I will not read the rest
+          break;
+
         }
         else // Skip lines for other snapshots
         {
