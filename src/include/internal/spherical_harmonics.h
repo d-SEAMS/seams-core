797afa1880ca7ca2163ddf29871814948eda5e4a
HaoZeke <rohit.goswami@aol.com>
2018-11-30 05:40:46 +0530

    spherical: Init with boost cruft

---
+++ src/include/internal/spherical_harmonics.h
@@ -0,0 +1,17 @@
+#ifndef _SPHERICAL_HARMONICS_H_
+#define _SPHERICAL_HARMONICS_H_
+
+#include <blaze/math/StaticVector.h>
+#include <boost/geometry.hpp>
+#include <boost/math/special_functions/spherical_harmonic.hpp>
+#include <cmath>
+#include <complex>
+
+// 7 is for Q3, orderL=3
+
+blaze::StaticVector<std::complex<double>, 7UL>
+spheriHarmo(int orderL, blaze::StaticVector<double, 2UL> coordSph);
+
+std::array<double, 2> radialCoord(std::array<double, 3>);
+
+#endif // _SPHERICAL_HARMONICS_H_
