186a076ae975ee8ca1de56937cfc891db06699f5
HaoZeke <rohit.goswami@aol.com>
2018-11-30 16:12:32 +0530

    namespace: Cheat for transition

--- src/transition.cpp
+++ src/transition.cpp
@@ -1,6 +1,8 @@
 #include "geometry.h"
 #include "transition.h"
 
+using namespace trans;
+
 /********************************************/ /**
  *  Constructor
  ***********************************************/
