daa42dfc1683eca7c16d4ee64634197778abcd5e
HaoZeke <rohit.goswami@aol.com>
2018-12-01 00:23:30 +0530

    neighbors: Finish testing, with access controls
    Co-authored-by: Amrita Goswami <amrita16thaug646@gmail.com>

--- tests/neighbors-test.cpp
+++ tests/neighbors-test.cpp
@@ -77,115 +77,13 @@ void generateRandomPointCloud(PointCloud<T> &point, const size_t N,
     point.pts[i].x = max_range * (rand() % 1000) / T(1000);
     point.pts[i].y = max_range * (rand() % 1000) / T(1000);
     point.pts[i].z = max_range * (rand() % 1000) / T(1000);
+    // Between -1 and 1
+    // point.pts[i].x = static_cast<T>(2 * (((double)rand()) / RAND_MAX) - 1);
+    // point.pts[i].y = static_cast<T>(2 * (((double)rand()) / RAND_MAX) - 1);
+    // point.pts[i].z = static_cast<T>(2 * (((double)rand()) / RAND_MAX) - 1);
   }
 }
 
-// This is an exampleof a custom data set class
-template <typename T> struct PointCloud_Quat {
-  struct Point {
-    T w, x, y, z;
-  };
-
-  std::vector<Point> pts;
-
-  // Must return the number of data points
-  inline size_t kdtree_get_point_count() const { return pts.size(); }
-
-  // Returns the dim'th component of the idx'th point in the class:
-  // Since this is inlined and the "dim" argument is typically an immediate value, the
-  //  "if/else's" are actually solved at compile time.
-  inline T kdtree_get_pt(const size_t idx, const size_t dim) const {
-    if (dim == 0)
-      return pts[idx].w;
-    else if (dim == 1)
-      return pts[idx].x;
-    else if (dim == 2)
-      return pts[idx].y;
-    else
-      return pts[idx].z;
-  }
-
-  // Optional bounding-box computation: return false to default to a standard bbox computation loop.
-  //   Return true if the BBOX was already computed by the class and returned in "bb" so it can be avoided to redo it again.
-  //   Look at bb.size() to find out the expected dimensionality (e.g. 2 or 3 for point clouds)
-  template <class BBOX> bool kdtree_get_bbox(BBOX & /* bb */) const {
-    return false;
-  }
-};
-
-template <typename T>
-void generateRandomPointCloud_Quat(PointCloud_Quat<T> &point, const size_t N) {
-  // Generating Random Quaternions
-  point.pts.resize(N);
-  T theta, X, Y, Z, sinAng, cosAng, mag;
-  for (size_t i = 0; i < N; i++) {
-    theta = static_cast<T>(nanoflann::pi_const<double>() *
-                           (((double)rand()) / RAND_MAX));
-    // Generate random value in [-1, 1]
-    X = static_cast<T>(2 * (((double)rand()) / RAND_MAX) - 1);
-    Y = static_cast<T>(2 * (((double)rand()) / RAND_MAX) - 1);
-    Z = static_cast<T>(2 * (((double)rand()) / RAND_MAX) - 1);
-    mag = sqrt(X * X + Y * Y + Z * Z);
-    X /= mag;
-    Y /= mag;
-    Z /= mag;
-    cosAng = cos(theta / 2);
-    sinAng = sin(theta / 2);
-    point.pts[i].w = cosAng;
-    point.pts[i].x = X * sinAng;
-    point.pts[i].y = Y * sinAng;
-    point.pts[i].z = Z * sinAng;
-  }
-}
-
-// This is an exampleof a custom data set class
-template <typename T> struct PointCloud_Orient {
-  struct Point {
-    T theta;
-  };
-
-  std::vector<Point> pts;
-
-  // Must return the number of data points
-  inline size_t kdtree_get_point_count() const { return pts.size(); }
-
-  // Returns the dim'th component of the idx'th point in the class:
-  // Since this is inlined and the "dim" argument is typically an immediate value, the
-  //  "if/else's" are actually solved at compile time.
-  inline T kdtree_get_pt(const size_t idx, const size_t dim = 0) const {
-    return pts[idx].theta;
-  }
-
-  // Optional bounding-box computation: return false to default to a standard bbox computation loop.
-  //   Return true if the BBOX was already computed by the class and returned in "bb" so it can be avoided to redo it again.
-  //   Look at bb.size() to find out the expected dimensionality (e.g. 2 or 3 for point clouds)
-  template <class BBOX> bool kdtree_get_bbox(BBOX & /* bb */) const {
-    return false;
-  }
-};
-
-template <typename T>
-void generateRandomPointCloud_Orient(PointCloud_Orient<T> &point,
-                                     const size_t N) {
-  // Generating Random Orientations
-  point.pts.resize(N);
-  for (size_t i = 0; i < N; i++) {
-    point.pts[i].theta = static_cast<T>(
-        (2 * nanoflann::pi_const<double>() * (((double)rand()) / RAND_MAX)) -
-        nanoflann::pi_const<double>());
-  }
-}
-
-void dump_mem_usage() {
-  FILE *f = fopen("/proc/self/statm", "rt");
-  if (!f)
-    return;
-  char str[300];
-  size_t n = fread(str, 1, 200, f);
-  str[n] = 0;
-  printf("MEM: %s\n", str);
-  fclose(f);
-}
 using namespace std;
 using namespace nanoflann;
 
@@ -195,52 +93,76 @@ template <typename num_t> void kdtree_demo(const size_t N) {
   // Generate points:
   generateRandomPointCloud(cloud, N);
 
-  num_t query_pt[3] = {0.5, 0.5, 0.5};
-
   // construct a kd-tree index:
   typedef KDTreeSingleIndexAdaptor<L2_Simple_Adaptor<num_t, PointCloud<num_t>>,
                                    PointCloud<num_t>, 3 /* dim */
                                    >
       my_kd_tree_t;
 
-  dump_mem_usage();
-
   my_kd_tree_t index(3 /*dim*/, cloud,
                      KDTreeSingleIndexAdaptorParams(10 /* max leaf */));
   index.buildIndex();
 
-  dump_mem_usage();
-  {
-    // do a knn search
-    const size_t num_results = 1;
-    size_t ret_index;
-    num_t out_dist_sqr;
-    nanoflann::KNNResultSet<num_t> resultSet(num_results);
-    resultSet.init(&ret_index, &out_dist_sqr);
-    index.findNeighbors(resultSet, &query_pt[0], nanoflann::SearchParams(10));
+#if 0
+	// Test resize of dataset and rebuild of index:
+	cloud.pts.resize(cloud.pts.size()*0.5);
+	index.buildIndex();
+#endif
 
-    std::cout << "knnSearch(nn=" << num_results << "): \n";
-    std::cout << "ret_index=" << ret_index << " out_dist_sqr=" << out_dist_sqr
-              << endl;
+  const num_t query_pt[3] = {4.4, 4.25, 9.4};
+
+  // ----------------------------------------------------------------
+  // knnSearch():  Perform a search for the N closest points
+  // ----------------------------------------------------------------
+  {
+    size_t num_results = 5;
+    std::vector<size_t> ret_index(num_results);
+    std::vector<num_t> out_dist_sqr(num_results);
+
+    num_results = index.knnSearch(&query_pt[0], num_results, &ret_index[0],
+                                  &out_dist_sqr[0]);
+
+    // In case of less points in the tree than requested:
+    ret_index.resize(num_results);
+    out_dist_sqr.resize(num_results);
+
+    cout << "knnSearch(): num_results=" << num_results << "\n";
+    for (size_t i = 0; i < num_results; i++)
+      cout << "idx[" << i << "]=" << ret_index[i] << " dist[" << i
+           << "]=" << out_dist_sqr[i] << endl;
+    cout << "\n";
+
+    cout << "point coordinates"
+         << "\n";
+    for (size_t i = 0; i < 3; i++)
+      cout << " coord[" << i << "]=" << cloud.kdtree_get_pt(ret_index[0], i);
+    cout << "\n";
   }
+
+  // ----------------------------------------------------------------
+  // radiusSearch(): Perform a search for the points within search_radius
+  // ----------------------------------------------------------------
   {
-    // Unsorted radius search:
-    const num_t radius = 1;
-    std::vector<std::pair<size_t, num_t>> indices_dists;
-    RadiusResultSet<num_t, size_t> resultSet(radius, indices_dists);
+    const num_t search_radius = static_cast<num_t>(0.5);
+    std::vector<std::pair<size_t, num_t>> ret_matches;
 
-    index.findNeighbors(resultSet, query_pt, nanoflann::SearchParams());
+    nanoflann::SearchParams params;
+    //params.sorted = false;
 
-    // Get worst (furthest) point, without sorting:
-    std::pair<size_t, num_t> worst_pair = resultSet.worst_item();
-    cout << "Worst pair: idx=" << worst_pair.first
-         << " dist=" << worst_pair.second << endl;
+    const size_t nMatches =
+        index.radiusSearch(&query_pt[0], search_radius, ret_matches, params);
+
+    cout << "radiusSearch(): radius=" << search_radius << " -> " << nMatches
+         << " matches\n";
+    for (size_t i = 0; i < nMatches; i++)
+      cout << "idx[" << i << "]=" << ret_matches[i].first << " dist[" << i
+           << "]=" << ret_matches[i].second << endl;
+    cout << "\n";
   }
 }
-
 TEST_CASE("Test nanoflann", "[nanoflann]") {
-  srand(static_cast<unsigned int>(time(nullptr)));
-  kdtree_demo<float>(1000000);
-  kdtree_demo<double>(1000000);
+  // srand(static_cast<unsigned int>(time(nullptr)));
+  // kdtree_demo<float>(1000000);
+  kdtree_demo<double>(10);
   REQUIRE(1 >= 0);
 }
