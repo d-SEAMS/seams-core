f6876c819dc040f0e845bc83077d3d31736df1a3
HaoZeke <rohit.goswami@aol.com>
2018-11-30 15:51:31 +0530

    spherical: Add trans namespace and some formatting

--- src/include/internal/spherical_harmonics.h
+++ src/include/internal/spherical_harmonics.h
@@ -1,11 +1,14 @@
 #ifndef _SPHERICAL_HARMONICS_H_
 #define _SPHERICAL_HARMONICS_H_
 
+#include <array>
 #include <blaze/math/StaticVector.h>
 #include <boost/geometry.hpp>
 #include <boost/math/special_functions/spherical_harmonic.hpp>
 #include <cmath>
 #include <complex>
+
+namespace trans {
 
 // 7 is for Q3, orderL=3
 
@@ -13,5 +16,7 @@ blaze::StaticVector<std::complex<double>, 7UL>
 spheriHarmo(int orderL, blaze::StaticVector<double, 2UL> coordSph);
 
 std::array<double, 2> radialCoord(std::array<double, 3>);
+
+} // namespace trans
 
 #endif // _SPHERICAL_HARMONICS_H_
