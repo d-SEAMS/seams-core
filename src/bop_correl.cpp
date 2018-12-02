c5311452269a2c8a01c2e9298547351f1ae71af2
HaoZeke <rohit.goswami@aol.com>
2018-12-02 18:10:17 +0530

    bop_correl: Init files

---
+++ src/bop_correl.cpp
@@ -0,0 +1,4 @@
+// For calculating the "correlation of orientational bond order parameter
+// developed by ten Wolde et al. based on the Steinhardt parameters"
+// Essentially the CHILL+ params by Molinero et al.
+#include <bop_correl.hpp>
---
+++ src/include/internal/bop_correl.hpp
@@ -0,0 +1,6 @@
+#ifndef __BOP_CORREL_H_
+#define __BOP_CORREL_H_
+
+#include <neighbors.hpp>
+
+#endif // __BOP_CORREL_H_
