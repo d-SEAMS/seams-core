4529e86d46d28526cbcd8c50ec6de449cee9f3ba
HaoZeke <rohit.goswami@aol.com>
2018-12-02 20:59:33 +0530

    bop_correl: Commit basic inherited class

--- src/bop_correl.cpp
+++ src/bop_correl.cpp
@@ -2,3 +2,31 @@
 // developed by ten Wolde et al. based on the Steinhardt parameters"
 // Essentially the CHILL+ params by Molinero et al.
 #include <bop_correl.hpp>
+
+/********************************************/ /**
+ *  Constructor
+ ***********************************************/
+
+//   this->frame = new CMolecularSystem;
+//   // Volume Limits
+//   this->coordLow = this->coordHigh = {0, 0, 0};
+// };
+
+// chill::bop::bop() {
+//   nop = -1;
+//   filename = nullptr;
+//   frameNum = -1;
+//   typeI = -1;
+// }
+
+/********************************************/ /**
+ *  Destructor
+ ***********************************************/
+
+// chill::bop::~bop() {}
+
+/********************************************/ /**
+ *  Public Functions
+ ***********************************************/
+
+// void chill::test::sim() { std::cout << "sim"; };
