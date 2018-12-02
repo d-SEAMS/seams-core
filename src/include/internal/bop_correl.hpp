2277154db64daaa73c0d901a17208a73b156b55a
HaoZeke <rohit.goswami@aol.com>
2018-12-02 20:59:33 +0530

    bop_correl: Commit basic inherited class

--- src/include/internal/bop_correl.hpp
+++ src/include/internal/bop_correl.hpp
@@ -3,4 +3,45 @@
 
 #include <neighbors.hpp>
 
+namespace chill {
+// Struct to hold coordinates, Cᵢⱼ, classifiers
+template <typename T> struct yodaCloud {
+  struct Point {
+    T x, y, z;
+    std::vector<T> cij;
+    std::vector<T> classifier;
+  };
+
+  std::vector<Point> pts;
+};
+
+// Input object
+
+template <typename T> struct initSlice {
+
+  std::array<T, 3> coordHigh = {0, 0, 0};
+  std::array<T, 3> coordLow = {0, 0, 0};
+  std::string filename = nullptr;
+  std::array<int, 2> frameRange = {0, 0};
+};
+
+class bop : public neigh::treeKNN {
+private:
+  int nop;
+  std::string filename;
+  int frameNum;
+  int typeI;
+
+public:
+  // Constructor
+  bop() : treeKNN() { std::cout << "die"; };
+  // Destructor (let the compiler figure it out DO NOT ALTER)
+  virtual ~bop() {}
+
+  // Function to generate a cloud of results
+  chill::yodaCloud<double> pointCIJ();
+  // Initializer to get stuff
+  int initBOP(chill::initSlice<double> starter);
+};
+} // namespace chill
 #endif // __BOP_CORREL_H_
