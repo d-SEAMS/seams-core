c94105b5c5d20fdcbaabbaee23113673c570ea2e
HaoZeke <rohit.goswami@aol.com>
2018-12-04 00:29:56 +0530

    bop: Add cubic ice tests
    Co-authored-by: Amrita Goswami <amrita16thaug646@gmail.com>

--- tests/bop_correl-test.cpp
+++ tests/bop_correl-test.cpp
@@ -8,7 +8,7 @@
 #include <catch2/catch.hpp>
 #include <rang.hpp>
 
-SCENARIO("Test the bond order correlation generation", "[bopCIJ]") {
+SCENARIO("Test the bond order correlation for BCC", "[bopMw]") {
   GIVEN("An object for initializing bop") {
     // DO NOT EDIT, FILE SPECIFIC
     std::unique_ptr<chill::bop> bopTest(new chill::bop);
@@ -23,6 +23,36 @@ SCENARIO("Test the bond order correlation generation", "[bopCIJ]") {
     // Fill starter
     starter.coordHigh = {3, 3, 3};
     starter.coordLow = {0, 0, 0};
+    starter.frameRange = {1, 2};
+    starter.filename = testFile;
+    WHEN("Initialze bop") {
+      bopTest->initBOP(num_of_points, atom_type, starter);
+      THEN("Something") {
+        // Hand calculate and test the Q_lm at pointQ(0)
+        // testPoint = bopTest->pointQ(0);
+        testPoint = bopTest->atomVerdict(0);
+        REQUIRE(1);
+      }
+    }
+    bopTest->cleanUp();
+  }
+}
+
+SCENARIO("Test the bond order correlation for Mw", "[bopMw]") {
+  GIVEN("An object for initializing bop") {
+    // DO NOT EDIT, FILE SPECIFIC
+    std::unique_ptr<chill::bop> bopTest(new chill::bop);
+    int atom_type = 1;
+    int frame_num = 1;
+    int num_of_neighbors = 4;
+    int num_of_points = 4096;
+    std::array<double, 3> query_point = {0, 0, 0};
+    std::string testFile = "../input/traj/mW_cubic.lammpstrj";
+    chill::initSlice<double> starter;
+    chill::yodaPoint<double> testPoint;
+    // Fill starter
+    // starter.coordHigh = {3, 3, 3};
+    // starter.coordLow = {0, 0, 0};
     starter.frameRange = {1, 2};
     starter.filename = testFile;
     WHEN("Initialze bop") {
