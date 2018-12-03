29c243fb1d2001cc149ab06d4e6e6c8d2997ca61
HaoZeke <rohit.goswami@aol.com>
2018-12-03 05:52:31 +0530

    bop: Per atom Q
    Co-authored-by: Amrita Goswami <amrita16thaug646@gmail.com>

--- tests/bop_correl-test.cpp
+++ tests/bop_correl-test.cpp
@@ -14,18 +14,23 @@ SCENARIO("Test the bond order correlation generation", "[bopCIJ]") {
     auto bopTest = new chill::bop;
     int atom_type = 1;
     int frame_num = 1;
-    int num_of_neighbors = 5;
+    int num_of_neighbors = 4;
     int num_of_points = 432;
+    std::array<double, 3> query_point = {0, 0, 0};
     std::string testFile = "../input/traj/dump-bcc.lammpstrj";
     chill::initSlice<double> starter;
+    chill::yodaPoint<double> testPoint;
     // Fill starter
     starter.coordHigh = {3, 3, 3};
-    starter.coordLow = {1, 1, 1};
+    starter.coordLow = {0, 0, 0};
     starter.frameRange = {1, 2};
     starter.filename = testFile;
     WHEN("Initialze bop") {
       bopTest->initBOP(num_of_points, atom_type, starter);
-      THEN("SOMETHING") { REQUIRE(1); }
+      THEN("Something") {
+        testPoint = bopTest->pointQ(0);
+        REQUIRE(1);
+      }
     }
   }
 }
