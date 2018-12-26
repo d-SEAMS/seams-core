d727e1c7b9f50b998388a8c7c0a22cd7eb087095
HaoZeke <rohit.goswami@aol.com>
2018-12-26 23:15:03 +0530

    lua: Complete lua passthrough

--- lua_inputs/iceType/functions.lua
+++ lua_inputs/iceType/functions.lua
@@ -1,7 +1,17 @@
 print("\n Welcome to the manual lua function evaluation environment.\n");
 
 dumpName="water.lammpstrj";
-x={0,0,0};
-resCloud=readFrame(trajectory, 1, resCloud, oxygenAtomType,false,x,x)
-print("\nWORM\n")
--- writeDump(resCloud,dumpName);
+slice={0,0,0}; --- This is not in use
+for frame=targetFrame,finalFrame,frameGap do
+   resCloud=readFrame(trajectory, frame, resCloud, oxygenAtomType,false,x,x) --- Get the frame
+   resCloud=neighborList(cutoffRadius, resCloud, oxygenAtomType); --- Calculate the neighborlist
+   resCloud=chillPlus_cij(resCloud,false); --- Calculate Cij (cloud,slice)
+   resCloud=chillPlus_iceType(resCloud,false,"Something"); --- Write out data (cloud,slice,name)
+   avgQ6=averageQ6(resCloud,false); --- Average Q6 (cloud,slice)
+   resCloud=modifyChill(resCloud,avgQ6); --- Modification (cloud,q6)
+   percentage_Ice(resCloud,false,"SomeOther"); --- Post reclassification writeOut
+   writeDump(resCloud,dumpName);
+   writeHistogram(resCloud,avgQ6);
+print("\nWORM\n");
+end
+print("\nWORM_PWNED\n");
--- lua_inputs/iceType/vars.lua
+++ lua_inputs/iceType/vars.lua
@@ -2,10 +2,10 @@ print("\n Welcome to the Ice Type Determination Module\n");
 cutoffRadius = 3.5; --- This is for H2O
 oxygenAtomType = 1; --- This is assigned by LAMMPS
 targetFrame=1; --- The first frame
-finalFrame=1; --- This is inclusive
+finalFrame=10; --- This is inclusive
 frameGap=1; --- The gap between frames
 dumpName="wat.lammpstrj"; --- Output file name
 
 --- DO NOT ENABLE if you have not read the instructions
-defineFunctions=false; --- The last test before all hell breaks loose
+defineFunctions=true; --- The last test before all hell breaks loose
 functionScript="../lua_inputs/iceType/functions.lua" --- This is relative to the binary location
