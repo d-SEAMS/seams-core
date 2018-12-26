4fe175481c0d3b7ca04337fac7b513a9729e3df7
HaoZeke <rohit.goswami@aol.com>
2018-12-26 15:13:15 +0530

    lua: Add basic functions and variables

---
+++ lua_inputs/iceType/vars.lua
@@ -0,0 +1,7 @@
+print("\n Welcome to the Ice Type Determination Module\n");
+cutoffRadius = 3.5; --- This is for H2O
+oxygenAtomType = 1; --- This is assigned by LAMMPS
+targetFrame=1; --- The first frame
+finalFrame=1; --- This is inclusive
+frameGap=1; --- The gap between frames
+dumpName="wat.lammpstrj";
