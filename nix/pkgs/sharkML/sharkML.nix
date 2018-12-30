d4f64ae45ab3566342b0e81ab62a5f1c3ecc8a9a
HaoZeke <rohit.goswami@aol.com>
2018-12-31 02:36:35 +0530

    nix: Update from org (sharkML)

---
+++ nix/pkgs/sharkML/sharkML.nix
@@ -0,0 +1,9 @@
+# [[file:~/Git/Github/C++/Forks/structureFactor/literateNix.org::*SharkML][SharkML:1]]
+# A standard cmake-based build
+{ stdenv, fetchJSON, cmake, boost, openblas, liblapack }:
+stdenv.mkDerivation {
+  name = "sharkML-master";
+  src = fetchJSON ./sharkML.src.json;
+  nativeBuildInputs = [ cmake boost openblas liblapack ];
+}
+# SharkML:1 ends here
