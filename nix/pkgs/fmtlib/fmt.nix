36918f47ed1bb6bb157b300a903eb3eeb9f7101a
HaoZeke <rohit.goswami@aol.com>
2018-12-30 23:57:04 +0530

    nix: Update from org

---
+++ nix/pkgs/fmtlib/fmt.nix
@@ -0,0 +1,12 @@
+# [[file:~/Git/Github/C++/Forks/structureFactor/literateNix.org::*FMT][FMT:1]]
+{ stdenv, fetchJSON, cmake }:
+stdenv.mkDerivation rec {
+  name = "fmtlib-master";
+  src = fetchJSON ./fmt.src.json;
+  nativeBuildInputs = [ cmake ];
+  meta = {
+    description = "{fmt} is an open-source formatting library for C++. It can be used as a safe and fast alternative to (s)printf and IOStreams.";
+    homepage = http://fmtlib.net;
+  };
+}
+# FMT:1 ends here
