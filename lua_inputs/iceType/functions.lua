691da72262db40625774a2aed05d23c17a211360
HaoZeke <rohit.goswami@aol.com>
2019-01-09 04:40:53 +0530

    proj: Clean legacy cruft

--- lua_inputs/iceType/functions.lua
+++ lua_inputs/iceType/functions.lua
@@ -34,10 +34,8 @@ for frame=targetFrame,finalFrame,frameGap do
    resCloud=readFrame(trajectory, frame, resCloud, oxygenAtomType,false,slice,slice) --- Get the frame
    resCloud=neighborList(cutoffRadius, resCloud, oxygenAtomType); --- Calculate the neighborlist
    resCloud=chillPlus_cij(resCloud,false); --- Calculate Cij (cloud,slice)
-   resCloud=chillPlus_iceType(resCloud,false,chillPlus_noMod); --- Write out data (cloud,slice,name)
    avgQ6=averageQ6(resCloud,false); --- Average Q6 (cloud,slice)
-   resCloud=modifyChill(resCloud,avgQ6); --- Modification (cloud,q6)
-   percentage_Ice(resCloud,false,chillPlus_mod); --- Post reclassification writeOut
+   percentage_Ice(resCloud,false,chillPlus_mod); ---  --- Write out data (cloud,slice,name)
    writeDump(resCloud,dumpName);
    writeHistogram(resCloud,avgQ6);
    --- Do the largest Ice cluster stuff
