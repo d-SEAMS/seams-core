f7abb2de1ca2976e7865f73aea0a510439612dc0
HaoZeke <rohit.goswami@aol.com>
2019-04-11 19:27:56 +0530

    shell: Add more details

--- shell.nix
+++ shell.nix
@@ -1,6 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
   # Define
   let
+    inherit (pkgs.lib) optional optionals;
   # Import
     buildpkgs = import ./nix {};
   in
@@ -8,5 +9,19 @@ pkgs.mkShell {
   # this will make all the build inputs from hello and gnutar
   # available to the shell environment
   inputsFrom =  [ buildpkgs.yodaStruct ];
-  # buildInputs = [ pkgs.hello ];
+  buildInputs = with pkgs; [
+    git
+  ]
+  ++ optional stdenv.isLinux glibcLocales # To allow setting consistent locale on linux
+  ++ optional stdenv.isLinux inotify-tools # For file_system
+  ++ optional stdenv.isLinux libnotify # For ExUnit
+  ;
+    # Set up environment vars
+    # We unset TERM b/c of https://github.com/NixOS/nix/issues/1056
+    shellHook = ''
+      stty sane
+      export TERM="xterm-256color"
+      export LANG="en_US.UTF-8"
+      export LC_ALL="en_US.UTF-8"
+    '';
 }
