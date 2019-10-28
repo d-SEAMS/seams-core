7ff576b35413ed48a1e17ec93dd271aae05c4278
HaoZeke <rohit.goswami@aol.com>
2019-10-28 19:36:29 +0530

    sh: Add a very basic testing script

---
+++ buildMe.sh
@@ -0,0 +1,8 @@
+if [[ "$PWD" =~ seams-core ]]; then
+    rm -rf build
+    mkdir build
+    cd build
+    cmake .. -DCMAKE_CXX_FLAGS="-pg -fsanitize=address " -DCMAKE_EXE_LINKER_FLAGS=-pg -DCMAKE_SHARED_LINKER_FLAGS=-pg
+    make
+    ./yodaStruct --script ../lua_inputs/transition_diff.lua -f ../lua_inputs/parameter.txt -c ../lua_inputs/config.yml
+fi
