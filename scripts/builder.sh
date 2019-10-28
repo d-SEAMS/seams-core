c8c9e80f251492fb76aa15a4932cec0d91f470ce
HaoZeke <rohit.goswami@aol.com>
2019-10-29 00:17:57 +0530

    builder: Try a different config

--- scripts/builder.sh
+++ scripts/builder.sh
@@ -6,7 +6,8 @@ if [[ "$PWD" =~ seams-core ]]; then
     rm -rf build
     mkdir build
     cd build
-    cmake .. -DCMAKE_CXX_FLAGS="-pg -fsanitize=address " -DCMAKE_EXE_LINKER_FLAGS=-pg -DCMAKE_SHARED_LINKER_FLAGS=-pg
-    make
+    # cmake .. -DCMAKE_CXX_FLAGS="-pg -fsanitize=address " -DCMAKE_EXE_LINKER_FLAGS=-pg -DCMAKE_SHARED_LINKER_FLAGS=-pg
+    cmake .. -DCMAKE_BUILD_TYPE=Debug
+    make -j4
     ./yodaStruct --script ../lua_inputs/transition_diff.lua -f ../lua_inputs/parameter.txt -c ../lua_inputs/config.yml
 fi
