3ce7b5b8eb69bda716b5de3dfb46a91fe01631f6
HaoZeke <rohit.goswami@aol.com>
2019-10-29 03:07:30 +0530

    builder: Stop wasting time

--- scripts/builder.sh
+++ scripts/builder.sh
@@ -9,5 +9,4 @@ if [[ "$PWD" =~ seams-core ]]; then
     # cmake .. -DCMAKE_CXX_FLAGS="-pg -fsanitize=address " -DCMAKE_EXE_LINKER_FLAGS=-pg -DCMAKE_SHARED_LINKER_FLAGS=-pg
     cmake .. -DCMAKE_BUILD_TYPE=Debug
     make -j4
-    ./yodaStruct --script ../lua_inputs/transition_diff.lua -f ../lua_inputs/parameter.txt -c ../lua_inputs/config.yml
 fi
