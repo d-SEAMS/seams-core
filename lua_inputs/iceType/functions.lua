b26249c05f08d4ec9f5c1ce11d052a4f34fa4d98
Amrita Goswami <amrita16thaug646@gmail.com>
2019-10-29 07:59:37 +0530

    Temporary comment outs

--- lua_inputs/iceType/functions.lua
+++ lua_inputs/iceType/functions.lua
@@ -34,18 +34,18 @@ io.close(tmpFileC);
 slice={0,0,0}; --- This is not in use
 for frame=targetFrame,finalFrame,frameGap do
    resCloud=readFrame(trajectory, frame, resCloud, oxygenAtomType,false,slice,slice) --- Get the frame
-   resCloud=neighborList(cutoffRadius, resCloud, oxygenAtomType); --- Calculate the neighborlist
-   resCloud=chillPlus_cij(resCloud,false); --- Calculate Cij (cloud,slice)
-   resCloud=chillPlus_iceType(resCloud,false,chillPlus_noMod); --- Write out data (cloud,slice,name)
-   writeDump(resCloud,dumpChillP); --- Dump the rescloud which currently has CHILL Plus classifications
-   avgQ6=averageQ6(resCloud,false); --- Average Q6 (cloud,slice)
-   resCloud=modifyChill(resCloud,avgQ6); --- Modification (cloud,q6)
-   percentage_Ice(resCloud,false,chillPlus_mod); --- Post reclassification writeOut
-   writeDump(resCloud,dumpSupaaP); --- Dump the rescloud which now has the supaa CHILL Plus Trajectory
-   writeHistogram(resCloud,avgQ6);
-   --- Do the largest Ice cluster stuff
-   clusterCloud=create_cluster(resCloud,clusterCloud);
-   largest_ice_cluster=largest_cluster(clusterCloud,cutoffRadius,true,false);
-   writeCluster(clusterCloud,largest_ice_cluster_name,false,largest_ice_cluster);
+   nList=neighborList(cutoffRadius, resCloud, oxygenAtomType); --- Calculate the neighborlist
+   -- resCloud=chillPlus_cij(resCloud,false); --- Calculate Cij (cloud,slice)
+   -- resCloud=chillPlus_iceType(resCloud,false,chillPlus_noMod); --- Write out data (cloud,slice,name)
+   -- writeDump(resCloud,dumpChillP); --- Dump the rescloud which currently has CHILL Plus classifications
+   -- avgQ6=averageQ6(resCloud,false); --- Average Q6 (cloud,slice)
+   -- resCloud=modifyChill(resCloud,avgQ6); --- Modification (cloud,q6)
+   -- percentage_Ice(resCloud,false,chillPlus_mod); --- Post reclassification writeOut
+   -- writeDump(resCloud,dumpSupaaP); --- Dump the rescloud which now has the supaa CHILL Plus Trajectory
+   -- writeHistogram(resCloud,avgQ6);
+   -- --- Do the largest Ice cluster stuff
+   -- clusterCloud=create_cluster(resCloud,clusterCloud);
+   -- largest_ice_cluster=largest_cluster(clusterCloud,cutoffRadius,true,false);
+   -- writeCluster(clusterCloud,largest_ice_cluster_name,false,largest_ice_cluster);
 end
 print("\nFinito\n");
