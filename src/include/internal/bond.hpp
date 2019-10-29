1852712030c4d50258a52a811c9c4f3df374292e
Amrita Goswami <amrita16thaug646@gmail.com>
2019-10-29 15:31:02 +0530

    bond: Declared a function for finding the hydrogen-bonded atoms of the neighbour list

--- src/include/internal/bond.hpp
+++ src/include/internal/bond.hpp
@@ -1,34 +1,43 @@
 #ifndef __BONDING_H_
 #define __BONDING_H_
 
+#include <math.h>
+#include <sys/stat.h>
 #include <algorithm>
 #include <array>
 #include <fstream>
 #include <iostream>
 #include <iterator>
-#include <math.h>
 #include <memory>
 #include <sstream>
 #include <string>
-#include <sys/stat.h>
 #include <vector>
 
+// Internal
 #include <cage.hpp>
 #include <mol_sys.hpp>
 
 namespace bond {
 
-// Create a vector of vectors containing bond connectivity information. May contain duplicates!
-// Gets the bond information from the vector of vectors containing the rings
-std::vector<std::vector<int>>
-populateBonds(std::vector<std::vector<int>> rings);
+// Create a vector of vectors (similar to the neighbour list conventions)
+// containing the hydrogen bond connectivity information. Decides the existence
+// of the hydrogen bond depending on the O--O and O--H vectors from the
+// neighbour list already constructed
+std::vector<std::vector<int>> populateHbonds(
+    molSys::PointCloud<molSys::Point<double>, double> *yCloud,
+    std::vector<std::vector<int>> nList, int Htype);
 
-// Creates a vector of vectors containing bond connectivity information from the rings vector of vectors
-// and cage information
-std::vector<std::vector<int>>
-createBondsFromCages(std::vector<std::vector<int>> rings,
-                     std::vector<cage::Cage> *cageList, cage::cageType type,
-                     int *nRings);
+// Create a vector of vectors containing bond connectivity information. May
+// contain duplicates! Gets the bond information from the vector of vectors
+// containing the rings
+std::vector<std::vector<int>> populateBonds(
+    std::vector<std::vector<int>> rings);
+
+// Creates a vector of vectors containing bond connectivity information from the
+// rings vector of vectors and cage information
+std::vector<std::vector<int>> createBondsFromCages(
+    std::vector<std::vector<int>> rings, std::vector<cage::Cage> *cageList,
+    cage::cageType type, int *nRings);
 
 // Remove duplicate bonds
 std::vector<std::vector<int>> trimBonds(std::vector<std::vector<int>> bonds);
@@ -42,10 +51,11 @@ int rmLongBonds(molSys::PointCloud<molSys::Point<double>, double> *yCloud,
                 std::vector<std::vector<int>> bonds, std::vector<bool> *flag,
                 double cutoff);
 
-// Search and set flag to false if a given 'bond' matches a bond in the vector of vectors
+// Search and set flag to false if a given 'bond' matches a bond in the vector
+// of vectors
 int searchBondMatch(std::vector<int> matchBond,
                     std::vector<std::vector<int>> bonds,
                     std::vector<bool> *flag);
-} // namespace bond
+}  // namespace bond
 
-#endif // __BONDING_H_
+#endif  // __BONDING_H_
