252c061617cd26b1d4632524be124731038b4137
Amrita Goswami <amrita16thaug646@gmail.com>
2019-10-29 15:31:40 +0530

    egLua: examples of pre-defined work-flows

---
+++ example_lua/chillPlus/iceType/vars.lua
@@ -0,0 +1,16 @@
+print("\n Welcome to the Ice Type Determination Module\n");
+cutoffRadius = 3.5; --- This is for H2O
+oxygenAtomType = 1; --- This is assigned by LAMMPS
+targetFrame=1; --- The first frame
+finalFrame=1; --- This is inclusive
+frameGap=1; --- The gap between frames
+dumpName="wat.lammpstrj"; --- Output file name
+chillPlus_mod="supaaChill.txt"; --- This the modified file
+chillPlus_noMod="chillPlus.txt"; --- This is the standard file name
+chill_noMod="chill.txt"; --- This is the standard file name
+largest_ice_cluster_name="largeCluster.txt"; --- This is the standard file name
+
+--- DO NOT ENABLE if you have not read the instructions
+defineFunctions=true; --- The last test before all hell breaks loose
+outDir="../runOne/"; --- The subdirectory used
+functionScript="../lua_inputs/iceType/functions.lua" --- This is relative to the binary location
