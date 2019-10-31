6a4f9dd672f83f11d627784ec31776782a91f527
Amrita Goswami <amrita16thaug646@gmail.com>
2019-10-31 08:25:45 +0530

    tests: Remove legacy code

--- tests/has_filesystem.cpp
+++
@@ -1,9 +0,0 @@
-#include <filesystem>
-
-namespace fs = std::filesystem;
-
-int main() {
-  fs::path aPath{"../"};
-
-  return 0;
-}
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,6 +1,7 @@
 /// C++ Structure Determination Toolkit
 ///
-/// Copyright © 2018-2019 Rohit Goswami <r95g10[at]gmail.com>, Amrita Goswami <amritag[at]iitk.ac.in>
+/// Copyright © 2018-2019 Rohit Goswami <r95g10[at]gmail.com>, Amrita Goswami
+/// <amritag[at]iitk.ac.in>
 ///
 /// Main Catch test driver
 ///
@@ -10,21 +11,19 @@
 #define CATCH_CONFIG_MAIN
 #include "catch2/catch.hpp"
 
-// #define CATCH_CONFIG_RUNNER
-// #define CATCH_CONFIG_NO_CPP17_UNCAUGHT_EXCEPTIONS
-// #define CATCH_CONFIG_CPP17_STRING_VIEW
-// #include <catch2/catch.hpp>
+#define CATCH_CONFIG_RUNNER
+#define CATCH_CONFIG_NO_CPP17_UNCAUGHT_EXCEPTIONS
+#define CATCH_CONFIG_CPP17_STRING_VIEW
+#include <catch2/catch.hpp>
 
-// int main(int argc, char* argv[])
-// {
+// int main(int argc, char* argv[]) {
 //   Catch::Session session;  // There must be exactly one instance
 
 //   // writing to session.configData() here sets defaults
 //   // this is the preferred way to set them
 
 //   int returnCode = session.applyCommandLine(argc, argv);
-//   if (returnCode != 0)
-//   {  // Indicates a command line error
+//   if (returnCode != 0) {  // Indicates a command line error
 //     return returnCode;
 //   }
 
