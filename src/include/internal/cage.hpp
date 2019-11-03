832d608894d0494ad609d6db75a8fb5a36ff4dac
Amrita Goswami <amrita16thaug646@gmail.com>
2019-11-03 15:44:56 +0530

    cage: Added enum for atom types

--- src/include/internal/cage.hpp
+++ src/include/internal/cage.hpp
@@ -8,14 +8,18 @@ namespace cage {
 // Type of a cage (a group of rings)
 enum cageType { HexC, DoubleDiaC, Mixed };
 
+// Type of ice for a particular atom. Dummy means that the atom is unclassified
+// and is most probably water
+enum iceType { dummy, hc, ddc, mixed };
+
 // Each DDC has one equatorial ring and 6 peripheral rings
 // Each HC has two basal planes and 3 prismatic planes
 // Struct for point type
 struct Cage {
-  cageType type;          // type of the cage : can be DDC or HC
-  std::vector<int> rings; // coordinates
+  cageType type;           // type of the cage : can be DDC or HC
+  std::vector<int> rings;  // coordinates
 };
 
-} // namespace cage
+}  // namespace cage
 
-#endif // __CAGE_H_
+#endif  // __CAGE_H_
