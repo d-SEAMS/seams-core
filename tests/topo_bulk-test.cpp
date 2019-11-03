67e780a8d26c2424045ff1d3b91312d7eb5d4eee
Amrita Goswami <amrita16thaug646@gmail.com>
2019-11-03 07:23:04 +0530

    topoTest: Added a successful test for DDCs

--- tests/topo_bulk-test.cpp
+++ tests/topo_bulk-test.cpp
@@ -130,3 +130,134 @@ SCENARIO("Test the HC algorithm for a single hexagonal cage.", "[topo]") {
     }  // End of getting the neighbour list
   }    // End of given
 }  // End of scenario
+
+// DDC determination
+SCENARIO("Test the DDC algorithm for a single double-diamond cage.", "[topo]") {
+  GIVEN("A pointCloud") {
+    // Hard-coded example of a single tetragonal prism
+    // Here, we manually input a tetragonal prism's coordinates
+    molSys::PointCloud<molSys::Point<double>, double> yCloud;  // pointCloud
+    molSys::Point<double> iPoint;                              // A single point
+    std::vector<double> box = {53.9690018, 54.5289994, 51.257};  // Box lengths
+    yCloud.box = box;  // Update the box lengths. z dim is axial
+    std::vector<std::vector<int>> nList;  // Neighbour list
+    std::vector<std::vector<int>> rings;  // Rings
+    std::vector<ring::strucType>
+        ringType;  // This vector will have a value for each ring inside
+    std::vector<int> listDDC;  // Contains atom indices of atoms making up HCs
+    // Make a list of all the DDCs and HCs
+    std::vector<cage::Cage> cageList;
+    // --------------------
+    // Building the double-diamond cage
+    //
+    iPoint.type = 1;  // Same for all the points here
+    // Element {0}
+    iPoint.atomID = 0;      // iatom
+    iPoint.x = 25.4799996;  // x
+    iPoint.y = 25.4799996;  // y
+    iPoint.z = 19.1100006;  // z
+    yCloud.pts.push_back(iPoint);
+    // Element {1}
+    iPoint.atomID = 1;      // iatom
+    iPoint.x = 25.4799996;  // x
+    iPoint.y = 25.4799996;  // y
+    iPoint.z = 25.4799996;  // z
+    yCloud.pts.push_back(iPoint);
+    // Element {2}
+    iPoint.atomID = 2;      // iatom
+    iPoint.x = 25.4799996;  // x
+    iPoint.y = 22.2950001;  // y
+    iPoint.z = 22.2950001;  // z
+    yCloud.pts.push_back(iPoint);
+    // Element {3}
+    iPoint.atomID = 3;      // iatom
+    iPoint.x = 25.4799996;  // x
+    iPoint.y = 28.6650009;  // y
+    iPoint.z = 22.2950001;  // z
+    yCloud.pts.push_back(iPoint);
+    // Element {4}
+    iPoint.atomID = 4;      // iatom
+    iPoint.x = 22.2950001;  // x
+    iPoint.y = 22.2950001;  // y
+    iPoint.z = 25.4799996;  // z
+    yCloud.pts.push_back(iPoint);
+    // Element {5}
+    iPoint.atomID = 5;      // iatom
+    iPoint.x = 22.2950001;  // x
+    iPoint.y = 25.4799996;  // y
+    iPoint.z = 22.2950001;  // z
+    yCloud.pts.push_back(iPoint);
+    // Element {6}
+    iPoint.atomID = 6;      // iatom
+    iPoint.x = 28.6650009;  // x
+    iPoint.y = 25.4799996;  // y
+    iPoint.z = 22.2950001;  // z
+    yCloud.pts.push_back(iPoint);
+    // Element {7}
+    iPoint.atomID = 7;      // iatom
+    iPoint.x = 23.8880006;  // x
+    iPoint.y = 20.7029991;  // y
+    iPoint.z = 23.8880006;  // z
+    yCloud.pts.push_back(iPoint);
+    // Element {8}
+    iPoint.atomID = 8;      // iatom
+    iPoint.x = 23.8880006;  // x
+    iPoint.y = 27.073;      // y
+    iPoint.z = 23.8880006;  // z
+    yCloud.pts.push_back(iPoint);
+    // Element {9}
+    iPoint.atomID = 9;      // iatom
+    iPoint.x = 27.073;      // x
+    iPoint.y = 27.073;      // y
+    iPoint.z = 20.7029991;  // z
+    yCloud.pts.push_back(iPoint);
+    // Element {10}
+    iPoint.atomID = 10;     // iatom
+    iPoint.x = 20.7029991;  // x
+    iPoint.y = 23.8880006;  // y
+    iPoint.z = 23.8880006;  // z
+    yCloud.pts.push_back(iPoint);
+    // Element {11}
+    iPoint.atomID = 11;     // iatom
+    iPoint.x = 27.073;      // x
+    iPoint.y = 23.8880006;  // y
+    iPoint.z = 23.8880006;  // z
+    yCloud.pts.push_back(iPoint);
+    // Element {12}
+    iPoint.atomID = 12;     // iatom
+    iPoint.x = 23.8880006;  // x
+    iPoint.y = 23.8880006;  // y
+    iPoint.z = 20.7029991;  // z
+    yCloud.pts.push_back(iPoint);
+    // Element {12}
+    iPoint.atomID = 13;     // iatom
+    iPoint.x = 23.8880006;  // x
+    iPoint.y = 23.8880006;  // y
+    iPoint.z = 27.073;      // z
+    yCloud.pts.push_back(iPoint);
+    // Update nop
+    yCloud.nop = yCloud.pts.size();
+    // Update the unordered map
+    for (int iatom = 0; iatom < yCloud.nop; iatom++) {
+      yCloud.idIndexMap[iatom] = iatom;
+    }  // end of filling the map
+    // --------------------
+    WHEN("Given a pointCloud") {
+      // Calculate a neighbour list
+      nList = nneigh::neighListO(3.5, &yCloud, 1);
+      // Neighbour list by index
+      nList = nneigh::neighbourListByIndex(&yCloud, nList);
+      // Find the vector of vector of rings
+      rings = primitive::ringNetwork(nList, 7);
+      THEN("There should be exactly one double-diamond cage.") {
+        ringType.resize(
+            rings.size());  // Has a value for each ring. init to zero.
+        // Find the number of hexagonal cages
+        listDDC = ring::findDDC(rings, &ringType, &cageList);
+        // Assert the number of cages
+        REQUIRE(cageList.size() ==
+                1);  // Evaluate condition for a single tetragonal prism
+      }
+    }  // End of getting the neighbour list
+  }    // End of given
+}  // End of scenario
