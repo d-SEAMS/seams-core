2822791d595dcd9a444e4ac7f2d05c37c415fd6f
Amrita Goswami <amrita16thaug646@gmail.com>
2019-11-04 18:04:48 +0530

    clump: Shifted all functions related to clustering to a new clump namespace and file

---
+++ src/include/internal/cluster.hpp
@@ -0,0 +1,53 @@
+#ifndef __CLUSTER_H_
+#define __CLUSTER_H_
+
+#include <math.h>
+#include <array>
+#include <boost/geometry.hpp>
+#include <boost/math/special_functions/spherical_harmonic.hpp>
+#include <bop.hpp>
+#include <cmath>
+#include <complex>
+#include <generic.hpp>
+#include <mol_sys.hpp>
+#include <neighbours.hpp>
+
+/*! \file cluster.hpp
+    \brief File for the bond order parameter analysis.
+
+    Details.
+*/
+
+/*!
+ *  \addtogroup clump
+ *  @{
+ */
+
+/*! \brief Clustering functions.
+ *         This namespace contains functions that are used in the
+for clustering ice-like particles.
+ *
+ Cluster analysis
+
+  ### Changelog ###
+
+  - Amrita Goswami [amrita16thaug646@gmail.com]; date modified: Nov 4, 2019
+ */
+
+namespace clump {
+
+// Finds the largest ice cluster
+int largestIceCluster(
+    molSys::PointCloud<molSys::Point<double>, double> *iceCloud, double cutoff,
+    bool printCluster = false, bool isSlice = false);
+
+// Does the cluster analysis of ice particles in the system. Returns a
+// pointCloud of the largest ice cluster.
+molSys::PointCloud<molSys::Point<double>, double> clusterAnalysis(
+    molSys::PointCloud<molSys::Point<double>, double> *iceCloud,
+    molSys::PointCloud<molSys::Point<double>, double> *yCloud,
+    std::vector<std::vector<int>> nList, std::string bopAnalysis = "q6");
+
+}  // namespace clump
+
+#endif  // __CLUSTER_H_
