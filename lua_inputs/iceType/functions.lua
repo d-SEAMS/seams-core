a6f0e76da8dfe9d04889ab5b7c806bc11f4078fa
Amrita Goswami <amrita16thaug646@gmail.com>
2019-11-16 17:54:21 +0530

    luaInp: init of monolayer stuff

--- lua_inputs/iceType/functions.lua
+++ lua_inputs/iceType/functions.lua
@@ -31,18 +31,10 @@ function make_output_dirs( doBOP, topoOneDim, topoTwoDim, topoBulk )
   end --- end of topo one dimensional dir
   -- Topological network criterion for quasi-two-dimensional INTs
   if not not topoTwoDim then
-    topoTwoDimDir = outDir .. "topoMonolayer/";
+    topoTwoDimDir = outDir .. "topoMonolayer";
     lfs.mkdir(topoTwoDimDir);
     topoTwoDimData = outDir .. "topoMonolayer/dataFiles";
     lfs.mkdir(topoTwoDimData);
-    --- Printing out cages per frame
-    if not not printCages then
-      for frame=targetFrame,finalFrame, frameGap
-       do
-        lfs.mkdir(topoTwoDimDir .. frame);
-      end --- end of for loop through frames
-    end --- End of making folders for every frame
-    ---
   end --- end of topo two dimensional dir
   -- Bulk topological network criterion 
   if not not topoBulk then
@@ -50,49 +42,19 @@ function make_output_dirs( doBOP, topoOneDim, topoTwoDim, topoBulk )
     lfs.mkdir(topoBulkDir);
     topoBulkDataDir = outDir .. "bulkTopo/dataFiles";
     lfs.mkdir(topoBulkDataDir);
-    -- Create file for cageData (no. of cages, rings etc.)
-    prismFileName = outDir .. "bulkTopo/cageData.dat";
-    prismFile=io.open(prismFileName, "w"); --- Allow overwriting (otherwise use a)
-    io.output(prismFile);
-    --- appends a word test to the last line of the file
-    io.write("Frame HCnumber DDCnumber MixedRingNumber PrismaticRings basalRings\n");
-    --- closes the open file
-    io.close(prismFile);
   end --- end of topo two dimensional dir
 end
 ---
-function clusterStatsFile()
-  -- Create the file
-  clusterFileName = outDir .. "clusterStats.dat";
-  clusterFile=io.open(clusterFileName, "w"); --- Allow overwriting (otherwise use a)
-    io.output(clusterFile);
-    --- appends a word test to the last line of the file
-    io.write("Frame largestCluster numOfClusters smallestCluster avgClusterSize\n");
-    --- closes the open file
-    io.close(clusterFile);
-end
 
---- Make the directories and files
+--- Make the directories
 make_output_dirs( doBOP, topoOneDim, topoTwoDim, topoBulk );
-clusterStatsFile(); --- For cluster stats file
 
-slice={0,0,0}; --- This is not in use
 for frame=targetFrame,finalFrame,frameGap do
-   resCloud=readFrameOnlyOne(trajectory,frame,resCloud,oxygenAtomType,false,slice,slice) --- Get the frame
+   resCloud=readFrameOnlyOne(trajectory,frame,resCloud,oxygenAtomType,isSlice,sliceLowerLimits,sliceUpperLimits) --- Get the frame
    nList=neighborList(cutoffRadius, resCloud, oxygenAtomType); --- Calculate the neighborlist by ID
-   ---
-   --- Since the bulk topological network criteria are slow for dense systems,
-   --- it is recommended to apply it on the largest ice cluster.
-   --- If you want to apply it on all the particles in the box, uncomment the following lines
-   --- and use resCloud as the pointCloud for the ring analyses. 
-   clusterAnalysis(outDir, clusterCloud, resCloud, nList, iceNeighbourList, cutoffRadius, "q6");
-   --- Recenter the cluster such that the centroid is at the center of the simulation box 
-   recenterCluster(clusterCloud, iceNeighbourList);
-   --- End of getting the largest ice cluster
-   ---
-   --- Start of analysis using rings (by index from here onwards.)
-   rings=getPrimitiveRings(iceNeighbourList,maxDepth); --- Gets every ring (non-primitives included)
-   bulkTopologicalNetworkCriterion(outDir, rings, iceNeighbourList, clusterCloud, printCages); --- Finds DDCs and HCs
-   --- Clean up for ice cluster and neighbour
+   hbnList=getHbondNetwork(trajectory,resCloud,nList,frame,hydrogenAtomType) --- Get the hydrogen-bonded network for the current frame
+   hbnList=bondNetworkByIndex(resCloud,hbnList) --- Hydrogen-bonded network using indices not IDs
+   rings=getPrimitiveRings(hbnList,maxDepth); --- Gets every ring (non-primitives included)
+   prismAnalysis(outDir, rings, hbnList, resCloud, maxDepth); --- Does the prism analysis for quasi-one-dimensional ice
 end
 print("\nFinito\n");
--- lua_inputs/iceType/vars.lua
+++ lua_inputs/iceType/vars.lua
@@ -1,11 +1,15 @@
 print("\n Welcome to the Ice Type Determination Module\n");
 cutoffRadius = 3.5; --- This is for H2O
-oxygenAtomType = 1; --- This is assigned by LAMMPS
--- hydrogenAtomType = 1; --- Hydrogen atom type assigned
-targetFrame=1; --- The first frame
-finalFrame=1; --- This is inclusive
+oxygenAtomType = 2; --- This is assigned by LAMMPS
+hydrogenAtomType = 1; --- Hydrogen atom type assigned
+targetFrame=1000; --- The first frame
+finalFrame=1000; --- This is inclusive
 frameGap=1; --- The gap between frames
-maxDepth = 7; --- The maximum depth upto which rings will be searched. 
+maxDepth = 5; --- The maximum depth upto which rings will be searched.
+--- Slice Information
+isSlice = true; --- This is true if the analysis is to be done only for a volume slice
+sliceLowerLimits = {0,0,0}; --- Lower limit of the slice (for box dim, keep the values the same as 0)
+sliceUpperLimits = {50,0,0}; --- Upper limit of the slice   
 
 --- DO NOT ENABLE if you have not read the instructions
 defineFunctions=true; --- The last test before all hell breaks loose
@@ -15,6 +19,8 @@ functionScript="../lua_inputs/iceType/functions.lua" --- This is relative to the
 --- Variables for creating directories, maybe shift to the config later?
 doBOP = false; --- Bond orienational parameter analysis (yes/no)
 topoOneDim = false; --- Topological network criterion for one-dimensional prisms
-topoTwoDim = false; --- Topological network criterion for two-dimensional ice
-topoBulk = true; --- DDC/HC criterion for bulk ice 
-printCages = false; --- Prints out every cage for every frame if true
+topoTwoDim = true; --- Topological network criterion for two-dimensional ice
+topoBulk = false; --- DDC/HC criterion for bulk ice
+
+--- Variables for the monolayer only:
+confiningSheetArea = 50*50; 
