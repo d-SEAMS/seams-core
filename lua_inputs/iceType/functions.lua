2cbbca9023558f118cafc410f4115c7df8743141
Amrita Goswami <amrita16thaug646@gmail.com>
2019-11-21 05:16:04 +0530

    separation attempted

--- lua_inputs/iceType/functions.lua
+++ lua_inputs/iceType/functions.lua
@@ -3,81 +3,28 @@ print("\n Welcome to the manual lua function evaluation environment.\n");
 --- Init Modules
 local lfs = require"lfs"
 
---- Functions for creating directories
-function make_output_dirs( doBOP, topoOneDim, topoTwoDim, topoBulk )
-  -- Make the main output folder:
-  lfs.mkdir(outDir);
-  -- Bond orientational parameter folder
-  if not not doBOP then
-    bopDir = outDir .. "bop";
-    lfs.mkdir(bopDir);
-  end --- end of bop dir
-  -- Topological network criterion for quasi-one-dimensional INTs
-  if not not topoOneDim then
-    topoOneDimDir = outDir .. "topoINT";
-    lfs.mkdir(topoOneDimDir);
-    topoOneDimDir = outDir .. "topoINT/prisms";
-    lfs.mkdir(topoOneDimDir);
-    topoOneDimData = outDir .. "topoINT/dataFiles";
-    lfs.mkdir(topoOneDimData);
-    -- Create file for nPrisms (no. of prisms)
-    prismFileName = outDir .. "topoINT/nPrisms.dat";
-    prismFile=io.open(prismFileName, "w"); --- Allow overwriting (otherwise use a)
-    io.output(prismFile);
-    --- appends a word test to the last line of the file
-    io.write("Frame RingSize Num_of_prisms Height% RingSize ... Height%\n");
-    --- closes the open file
-    io.close(prismFile);
-  end --- end of topo one dimensional dir
-  -- Topological network criterion for quasi-two-dimensional INTs
-  if not not topoTwoDim then
-    topoTwoDimDir = outDir .. "topoMonolayer";
-    lfs.mkdir(topoTwoDimDir);
-    topoTwoDimData = outDir .. "topoMonolayer/dataFiles";
-    lfs.mkdir(topoTwoDimData);
-    -- Create file for coverageAreaXY.dat 
-    areaFileName = outDir .. "topoMonolayer/coverageAreaXY.dat";
-    areaFile=io.open(areaFileName, "w"); --- Allow overwriting (otherwise use a)
-    io.output(areaFile);
-    --- appends a word test to the last line of the file
-    io.write("Frame RingSize Num_of_rings CoverageAreaXY% RingSize ... CoverageAreaXY%\n");
-    --- closes the open file
-    io.close(areaFile);
-    -- Create file for coverageAreaXZ.dat 
-    areaFileNameXZ = outDir .. "topoMonolayer/coverageAreaXZ.dat";
-    areaFileXZ=io.open(areaFileNameXZ, "w"); --- Allow overwriting (otherwise use a)
-    io.output(areaFileXZ);
-    --- appends a word test to the last line of the file
-    io.write("Frame RingSize Num_of_rings CoverageAreaXZ% RingSize ... CoverageAreaXZ%\n");
-    --- closes the open file
-    io.close(areaFileXZ);
-    -- Create file for coverageAreaYZ.dat 
-    areaFileNameYZ = outDir .. "topoMonolayer/coverageAreaYZ.dat";
-    areaFileYZ=io.open(areaFileNameYZ, "w"); --- Allow overwriting (otherwise use a)
-    io.output(areaFileYZ);
-    --- appends a word test to the last line of the file
-    io.write("Frame RingSize Num_of_rings CoverageAreaYZ% RingSize ... CoverageAreaYZ%\n");
-    --- closes the open file
-    io.close(areaFileYZ);
-  end --- end of topo two dimensional dir
-  -- Bulk topological network criterion 
-  if not not topoBulk then
-    topoBulkDir = outDir .. "bulkTopo";
-    lfs.mkdir(topoBulkDir);
-    topoBulkDataDir = outDir .. "bulkTopo/dataFiles";
-    lfs.mkdir(topoBulkDataDir);
-  end --- end of topo two dimensional dir
-end
----
+--- Call functions defined in script file
+local luaFunctions = require("luaFunctions.scripts");
 
 --- Make the directories
-make_output_dirs( doBOP, topoOneDim, topoTwoDim, topoBulk );
+luaFunctions.make_output_dirs( doBOP, topoOneDim, topoTwoDim, topoBulk );
+luaFunctions.clusterStatsFile();
 
 for frame=targetFrame,finalFrame,frameGap do
-   resCloud=readFrameOnlyOne(trajectory,frame,resCloud,oxygenAtomType,isSlice,sliceLowerLimits,sliceUpperLimits) --- Get the frame
+   resCloud=readFrame(trajectory,frame,resCloud,oxygenAtomType,isSlice,sliceLowerLimits,sliceUpperLimits) --- Get the frame
    nList=neighborList(cutoffRadius, resCloud, oxygenAtomType); --- Calculate the neighborlist by ID
-   hbnList=getHbondNetwork(trajectory,resCloud,nList,frame,hydrogenAtomType) --- Get the hydrogen-bonded network for the current frame
-   hbnList=bondNetworkByIndex(resCloud,hbnList) --- Hydrogen-bonded network using indices not IDs
-   rings=getPrimitiveRings(hbnList,maxDepth); --- Gets every ring (non-primitives included)
-   ringAnalysis(outDir, rings, hbnList, resCloud, maxDepth, confiningSheetArea); --- Does the ring analysis for quasi-two-dimensional ice
+   ---
+   resCloud=chillPlus_cij(resCloud,nList,isSlice); --- Calculate Cij (cloud,slice)
+   resCloud=chillPlus_iceType(resCloud,nList,outDir,isSlice,chillPlus_noMod); --- Write out data (cloud,slice,name)
+   writeDump(resCloud,outDir,dumpChillP); --- Dump the rescloud which currently has CHILL Plus classifications
+   --- Modified CHILL+
+   avgQ6=averageQ6(resCloud,nList,isSlice); --- Average Q6 (cloud,slice)
+   resCloud=modifyChill(resCloud,avgQ6); --- Modification (cloud,q6)
+   percentage_Ice(resCloud,outDir,isSlice,chillPlus_mod); --- Post reclassification writeOut
+   writeDump(resCloud,outDir,dumpSupaaP); --- Dump the rescloud which now has the supaa CHILL Plus Trajectory
+   --- Get the largest ice cluster. Here, iceNeighbourList is the neighbour list by index.
+   clusterAnalysis(outDir, clusterCloud, resCloud, nList, iceNeighbourList, cutoffRadius, "q6");
+   --- Recenter the cluster such that the centroid is at the center of the simulation box 
+   recenterCluster(clusterCloud, iceNeighbourList);
+   writeDump(resCloud,outDir,largestClusterDump); --- Dump the recentered largest ice cluster
 end
--- lua_inputs/iceType/vars.lua
+++ lua_inputs/iceType/vars.lua
@@ -1,19 +1,29 @@
 print("\n Welcome to the Ice Type Determination Module\n");
 cutoffRadius = 3.5; --- This is for H2O
-oxygenAtomType = 2; --- This is assigned by LAMMPS
-hydrogenAtomType = 1; --- Hydrogen atom type assigned
-targetFrame=1000; --- The first frame
-finalFrame=1000; --- This is inclusive
+oxygenAtomType = 1; --- This is assigned by LAMMPS
+hydrogenAtomType = 1; --- Hydrogen atom type assigned (not used here)
+targetFrame=1; --- The first frame
+finalFrame=1; --- This is inclusive
 frameGap=1; --- The gap between frames
-maxDepth = 4; --- The maximum depth upto which rings will be searched.
+maxDepth = 6; --- The maximum depth upto which rings will be searched.
 --- Slice Information
-isSlice = true; --- This is true if the analysis is to be done only for a volume slice
+isSlice = false; --- This is true if the analysis is to be done only for a volume slice
 sliceLowerLimits = {0,0,0}; --- Lower limit of the slice (for box dim, keep the values the same as 0)
-sliceUpperLimits = {50,0,0}; --- Upper limit of the slice   
+sliceUpperLimits = {0,0,0}; --- Upper limit of the slice   
 
 --- Paths for the output directories and lua scipt
-outDir="../runOne/"; --- The subdirectory used; 
+outDir="../runOne/"; --- The subdirectory used; Keep the slash at the end 
 functionScript="../lua_inputs/iceType/functions.lua" --- This is relative to the binary location
 
---- Variables for the monolayer only:
-confiningSheetArea = 50*50; 
+-- Variable for the topological network criterion
+printCages = false; --- Prints out every cage for every frame if true
+
+-- File names for outputs 
+dumpName="wat.lammpstrj"; --- Output file name
+chillPlus_mod="supaaChill.txt"; --- This the modified file
+chillPlus_noMod="chillPlus.txt"; --- This is the standard file name
+chill_noMod="chill.txt"; --- This is the standard file name
+largest_ice_cluster_name="largeCluster.txt"; --- This is the standard file name 
+dumpChillP= "waterChillP.lammpstrj"; --- Output dump file for CHILL+ classification
+dumpSupaaP= "waterSupaaP.lammpstrj"; --- Output dump file for the SUPER CHILL+ classification
+largestClusterDump = "largestIce.lammpstrj"; --- Output dump file for the largest ice cluster
