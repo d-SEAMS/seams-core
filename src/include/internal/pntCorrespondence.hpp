b556bff03ce0c4a25dcd90dc81a2fc11ed59c76f
Amrita Goswami <amrita16thaug646@gmail.com>
2019-12-14 10:01:30 +0530

    Correspondence: Helper functions for getting the point-to-point correspondence.

---
+++ src/include/internal/pntCorrespondence.hpp
@@ -0,0 +1,30 @@
+#ifndef __PNTCORRESPONDENCE_H_
+#define __PNTCORRESPONDENCE_H_
+
+#include <math.h>
+#include <sys/stat.h>
+#include <algorithm>
+#include <array>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <vector>
+
+// External
+#include <eigen3/Eigen/Core>
+
+#include <cage.hpp>
+#include <mol_sys.hpp>
+#include <seams_input.hpp>
+#include <seams_output.hpp>
+
+namespace pntToPnt {
+
+//
+
+}  // namespace pntToPnt
+
+#endif  // __PNTCORRESPONDENCE_H_
---
+++ src/pntCorrespondence.cpp
@@ -0,0 +1,1 @@
+#include <pntCorrespondence.hpp>
