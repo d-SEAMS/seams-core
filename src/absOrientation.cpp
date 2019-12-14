16fee4f4d2215a6493579c7c98b90e9cd4566ba3
Amrita Goswami <amrita16thaug646@gmail.com>
2019-12-14 08:01:55 +0530

    abOrient: Init of absolute orientation using Horn's algorithm

---
+++ src/absOrientation.cpp
@@ -0,0 +1,7 @@
+#include <absOrientation.hpp>
+
+/********************************************/ /**
+*  Deletes the memory of a
+ vector of vectors (specifically for rings).
+ *  @param[in, out] rings The vector of vectors to be cleared.
+***********************************************/
---
+++ src/include/internal/absOrientation.hpp
@@ -0,0 +1,28 @@
+#ifndef __ABSORIENTATION_H_
+#define __ABSORIENTATION_H_
+
+#include <math.h>
+#include <sys/stat.h>
+#include <algorithm>
+#include <array>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include <mol_sys.hpp>
+#include <quaternion.hpp>
+#include <ring.hpp>
+#include <seams_input.hpp>
+#include <seams_output.hpp>
+
+namespace absor {
+
+//
+
+}  // namespace absor
+
+#endif  // __ABSORIENTATION_H_
