cc3c0096b5748419a7bb6d4b7f1fdbeaa68ef7ce
Amrita Goswami <amrita16thaug646@gmail.com>
2019-12-16 03:26:31 +0530

    match: init

---
+++ src/include/internal/shapeMatch.hpp
@@ -0,0 +1,29 @@
+#ifndef __SHAPEMATCH_H_
+#define __SHAPEMATCH_H_
+
+#include <math.h>
+#include <sys/stat.h>
+#include <algorithm>
+#include <array>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include <absOrientation.hpp>
+#include <mol_sys.hpp>
+#include <pntCorrespondence.hpp>
+#include <ring.hpp>
+#include <seams_input.hpp>
+#include <seams_output.hpp>
+
+namespace match {
+
+//
+
+}  // namespace match
+
+#endif  // __SHAPEMATCH_H_
