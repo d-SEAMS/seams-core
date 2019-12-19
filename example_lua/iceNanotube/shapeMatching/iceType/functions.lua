92a9e611c1898a1211814fcf111b11a16ad3f010
Amrita Goswami <amrita16thaug646@gmail.com>
2019-12-19 17:48:55 +0530

    examples: Update for shape matching

---
+++ example_lua/iceNanotube/shapeMatching/iceType/functions.lua
@@ -0,0 +1,20 @@
+print("\n Welcome to the manual lua function evaluation environment.\n");
+
+--- Init Modules
+local lfs = require"lfs"
+
+--- Call functions defined in script file
+package.path = './../lua_inputs/luaModules/?.lua;' .. package.path
+local luaFunctions = require("scripts");
+
+--- Make the directories
+luaFunctions.make_output_dirs( doBOP, topoOneDim, topoTwoDim, topoBulk );
+
+for frame=targetFrame,finalFrame,frameGap do
+   resCloud=readFrameOnlyOne(trajectory,frame,resCloud,oxygenAtomType,isSlice,sliceLowerLimits,sliceUpperLimits) --- Get the frame
+   nList=neighborList(cutoffRadius, resCloud, oxygenAtomType); --- Calculate the neighborlist by ID
+   hbnList=getHbondNetwork(trajectory,resCloud,nList,frame,hydrogenAtomType) --- Get the hydrogen-bonded network for the current frame
+   hbnList=bondNetworkByIndex(resCloud,hbnList) --- Hydrogen-bonded network using indices not IDs
+   rings=getPrimitiveRings(hbnList,maxDepth); --- Gets every ring (non-primitives included)
+   prismAnalysis(outDir, rings, hbnList, resCloud, maxDepth, doShapeMatching); --- Does the prism analysis for quasi-one-dimensional ice
+end
