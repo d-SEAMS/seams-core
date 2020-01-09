a7a90a8aad885eb3136fc444944249a515673264
Rohit Goswami <rohit.goswami@aol.com>
2020-01-09 20:33:56 +0000

    shell: Use lua

--- shell.nix
+++ shell.nix
@@ -16,6 +16,7 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     gdb
     git
+    lua
     luaPackages.luafilesystem
   ]
   ++ optional stdenv.isLinux glibcLocales # To allow setting consistent locale on linux
