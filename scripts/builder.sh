971d7193635b56e0b8f9549bd57233468448aeff
Rohit Goswami <rohit.goswami@aol.com>
2020-01-10 20:08:27 +0000

    builder: Update to prevent clashes with nix-env -i

--- scripts/builder.sh
+++ scripts/builder.sh
@@ -3,9 +3,9 @@
 topLevel=$(git rev-parse --show-toplevel)
 cd $topLevel
 if [[ "$PWD" =~ seams-core ]]; then
-	rm -rf build
-	mkdir build
-	cd build
+	rm -rf shellBuild
+	mkdir shellBuild
+	cd shellBuild
 	# cmake .. -DCMAKE_CXX_FLAGS="-pg -fsanitize=address " -DCMAKE_EXE_LINKER_FLAGS=-pg -DCMAKE_SHARED_LINKER_FLAGS=-pg
 	cmake .. -DCMAKE_BUILD_TYPE=Debug
 	# cmake .. -DCMAKE_BUILD_TYPE=Release
