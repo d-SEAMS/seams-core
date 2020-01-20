afdf431aa95e034e64c26c1761d409bb4ed15adf
Amrita Goswami <amrita16thaug646@gmail.com>
2020-01-21 05:24:09 +0530

    luaInp: Shape matching used for the example

--- lua_inputs/iceType/functions.lua
+++ lua_inputs/iceType/functions.lua
@@ -6,5 +6,5 @@ for frame=targetFrame,finalFrame,frameGap do
    hbnList=getHbondNetwork(trajectory,resCloud,nList,frame,hydrogenAtomType) --- Get the hydrogen-bonded network for the current frame
    hbnList=bondNetworkByIndex(resCloud,hbnList) --- Hydrogen-bonded network using indices not IDs
    rings=getPrimitiveRings(hbnList,maxDepth); --- Gets every ring (non-primitives included)
-   prismAnalysis(outDir, rings, hbnList, resCloud, maxDepth, targetFrame); --- Does the prism analysis for quasi-one-dimensional ice
+   prismAnalysis(outDir, rings, hbnList, resCloud, maxDepth, lowestAtomID, targetFrame, frame, doShapeMatching); --- Does the prism analysis for quasi-one-dimensional ice
 end
