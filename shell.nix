2e025e2975dd588a44743a4f2a2f9bff3875eb55
Rohit Goswami <rgoswami@ieee.org>
2020-06-06 18:11:11 +0000

    nix: Pin versions with Niv

--- shell.nix
+++ shell.nix
@@ -1,34 +1,27 @@
-{ pkgs ? import <nixpkgs> {} }:
-  # Define
-  let
-    inherit (pkgs.lib) optional optionals;
+# Define
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { };
+  inherit (pkgs.lib) optional optionals;
   # Import
-    buildpkgs = import ./nix {};
-  in
-pkgs.mkShell {
-  # this will make all the build inputs from hello and gnutar
+  buildpkgs = import ./nix { };
+in pkgs.mkShell {
+  # this will make all the build inputs described
   # available to the shell environment
-  inputsFrom =  [
-    buildpkgs.yodaStruct
-    pkgs.git
-                ];
-  nativeBuildInputs = with pkgs; [pkgconfig];
-  buildInputs = with pkgs; [
-    gdb
-    git
-    lua
-    luaPackages.luafilesystem
-  ]
-  ++ optional stdenv.isLinux glibcLocales # To allow setting consistent locale on linux
-  ++ optional stdenv.isLinux inotify-tools # For file_system
-  ++ optional stdenv.isLinux libnotify # For ExUnit
+  inputsFrom = [ buildpkgs.yodaStruct pkgs.git ];
+  nativeBuildInputs = with pkgs; [ pkgconfig ];
+  buildInputs = with pkgs;
+    [ gdb git lua luaPackages.luafilesystem ] ++ optional stdenv.isLinux
+    glibcLocales # To allow setting consistent locale on linux
+    ++ optional stdenv.isLinux inotify-tools # For file_system
+    ++ optional stdenv.isLinux libnotify # For ExUnit
   ;
-    # Set up environment vars
-    # We unset TERM b/c of https://github.com/NixOS/nix/issues/1056
-    shellHook = ''
-      stty sane
-      export TERM="xterm-256color"
-      export LANG="en_US.UTF-8"
-      export LC_ALL="en_US.UTF-8"
-    '';
+  # Set up environment vars
+  # We unset TERM b/c of https://github.com/NixOS/nix/issues/1056
+  shellHook = ''
+    stty sane
+    export TERM="xterm-256color"
+    export LANG="en_US.UTF-8"
+    export LC_ALL="en_US.UTF-8"
+  '';
 }
