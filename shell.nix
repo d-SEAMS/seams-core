8a12046b74a92120f6f3b1e6db5d96d1ddbe0a5e
Rohit Goswami <rgoswami@ieee.org>
2021-03-18 00:01:36 +0000

    nix: Add doxygen for gha

--- shell.nix
+++ shell.nix
@@ -5,13 +5,14 @@ let
   inherit (pkgs.lib) optional optionals;
   # Import
   buildpkgs = import ./nix { };
+  doxygen = pkgs.doxygen.overrideAttrs (_: { version = "1.9.1"; });
 in pkgs.mkShell {
   # this will make all the build inputs described
   # available to the shell environment
   inputsFrom = [ buildpkgs.yodaStruct pkgs.git ];
   nativeBuildInputs = with pkgs; [ pkgconfig ];
   buildInputs = with pkgs;
-    [ gdb git lua luaPackages.luafilesystem ] ++ optional stdenv.isLinux
+    [ gdb git lua luaPackages.luafilesystem doxygen ] ++ optional stdenv.isLinux
     glibcLocales # To allow setting consistent locale on linux
     ++ optional stdenv.isLinux inotify-tools # For file_system
     ++ optional stdenv.isLinux libnotify # For ExUnit
