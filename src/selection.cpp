33895fce36a67eb2910939d0ee8cbfa547d7db30
amritagos <amrita16thaug646@gmail.com>
2022-09-06 14:36:34 +0000

    selection: Added function for setting inSlice to true for all atoms in a single slice. Atoms belonging to the same molecule are included in the slice if even one of the constituent atoms is in the slice

--- src/selection.cpp
+++ src/selection.cpp
@@ -19,19 +19,6 @@
  * @details Function that loops through a given input pointCloud and 
  * returns a new pointCloud only containing atoms of a given atom type ID.  
  * This is registered as a Lua function, and is exposed to the user directly. 
- * The function calls the following functions internally:
- * - ring::clearRingList (Clears the vector of vectors for rings of a single
- * type, to prevent excessive memory being blocked).
- * - ring::getSingleRingSize (Fill a vector of vectors for rings of a particular
- * ring size).
- * - ring::findPrisms (Now that rings of a particular size have been obtained,
- * prism blocks are found and saved).
- * - topoparam::normHeightPercent (Gets the height% for the prism blocks).
- * - ring::assignPrismType (Assigns a prism type to each atom type).
- * - sout::writePrismNum (Write out the prism information for the current
- * frame).
- * - sout::writeLAMMPSdataAllPrisms (Writes out a LAMMPS data file for the
- * current frame, which can be visualized in OVITO).
  * @param[in] yCloud The given input PointCloud
  * @param[out] outCloud The output PointCloud
  * @param[in] atomTypeI The type ID of the atoms to save in the output PointCloud
@@ -92,4 +79,77 @@ gen::getPointCloudOneAtomType(
   outCloud->currentFrame = yCloud->currentFrame; 
 
   return *outCloud;
+}
+
+/**
+ * @details Function that loops through a given input PointCloud and 
+ * sets the inSlice bool for every Point according to whether the molecule  
+ * is in the specified (single) slice or not. If even one atom of a molecule 
+ * is inside the region, then all atoms belonging to that molecule should be
+ * inside the slice as well (therefore, inSlice would be set to true)
+ * @param[in] yCloud The given input PointCloud
+ * @param[in] coordLow Contains the lower limits of the slice, if a slice is to
+ *  be created
+ * @param[in] coordHigh Contains the upper limits of the slice, if a slice is
+ *  to be created
+ */
+molSys::PointCloud<molSys::Point<double>, double>
+gen::moleculesInSingleSlice(
+    molSys::PointCloud<molSys::Point<double>, double> *yCloud,
+    std::array<double, 3> coordLow,
+    std::array<double, 3> coordHigh) {
+  //
+  bool pointInSlice = true; // If the current point is in the slice, this is true (otherwise this is false)
+  std::unordered_multimap<int, int>
+      molIDAtomIDmap; // Unordered multimap with molecule IDs of the atoms as the keys and the
+                  // atom IDs as the values. More than one atom can have the same molecule ID
+  int iatomMolID; // molID of the current iatom
+  int jatomID;    // atom ID of the current jatom
+  int jatomIndex; // index of jatom
+
+  // --------------------
+  // Get the unordered map of the atom IDs (keys) and the molecular IDs
+  // (values)
+  molIDAtomIDmap = molSys::createMolIDAtomIDMultiMap(yCloud);
+  // --------------------
+  // Set inSlice to false for every atom first
+  for (int iatom = 0; iatom < yCloud->nop; iatom++) {
+    yCloud->pts[iatom].inSlice = false;
+  } // end of init 
+  // --------------------
+
+  // Loop through every iatom and check if the atom is in the slice or not 
+  for (int iatom = 0; iatom < yCloud->nop; iatom++) {
+    // Check if the current atom is in the slice 
+    pointInSlice = sinp::atomInSlice(yCloud->pts[iatom].x, yCloud->pts[iatom].y, yCloud->pts[iatom].z,
+                                               coordLow, coordHigh);
+    //
+    // Set the inSlice bool for the particular Point 
+    if (pointInSlice)
+    {
+      yCloud->pts[iatom].inSlice = true; // iatom is inside the slice
+      // Find mol ID and atoms with that particular molecular ID 
+      iatomMolID = yCloud->pts[iatom].molID; // molecule ID 
+      // -----------
+      // Find all atoms with iatomMolID and set the inSlice bool
+      // to true
+      auto range = molIDAtomIDmap.equal_range(iatomMolID); 
+      // Loop through all atoms with iatomMolID
+      for (auto it = range.first; it != range.second; it++)
+      {
+        // it->second gives the value (in this case, the atom ID)
+        jatomID = it->second; // Atom ID with molecule ID equal to iatomMolID
+        auto gotJ = yCloud->idIndexMap.find(jatomID);
+        jatomIndex = gotJ->second;
+        // Set the jatom inSlice bool to true
+        yCloud->pts[jatomIndex].inSlice = true; // jatomIndex is inside the slice 
+      }
+      // -----------
+    } // the atom is in the slice 
+    else{
+      yCloud->pts[iatom].inSlice = false; // iatom is not in the slice  
+    } // atom is not in the slice
+  }
+
+  return *yCloud;
 }
