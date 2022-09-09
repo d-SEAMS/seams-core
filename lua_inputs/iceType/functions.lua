6a6826208cd5559213a499163530d9a906fe1d3d
amritagos <amrita16thaug646@gmail.com>
2022-09-09 14:48:05 +0000

    lua_inputs: One function now handles the selection operations

--- lua_inputs/iceType/functions.lua
+++ lua_inputs/iceType/functions.lua
@@ -7,14 +7,12 @@ for frame=targetFrame,finalFrame,frameGap do
     oCloud=getPointCloudAtomsOfOneAtomType(resCloud,oCloud,oxygenAtomType,false, {0,0,0}, {0,0,0});
     -- Get the H atoms (including those not in the slice) for the entire system  
     hCloud=getPointCloudAtomsOfOneAtomType(resCloud,hCloud,hydrogenAtomType,false,{0,0,0}, {0,0,0});
-    nList=neighborList(cutoffRadius, oCloud, oxygenAtomType); --- Calculate the neighborlist by ID (check this)
+    nList=neighborList(cutoffRadius, oCloud, oxygenAtomType); --- Calculate the neighborlist by ID
     hbnList=getHbondNetworkFromClouds(oCloud,hCloud,nList); --- Get the hydrogen-bonded network for the current frame
     hbnList=bondNetworkByIndex(oCloud,hbnList); --- Hydrogen-bonded network using indices not IDs
     rings=getPrimitiveRings(hbnList,maxDepth); --- Gets every ring (non-primitives included)
     bulkRingNumberAnalysis(outDir, rings, hbnList, oCloud, maxDepth, targetFrame); --- Writes out primitive rings for a bulk system
     -- All atoms within the slice will have an inSlice bool attribute of true. If a single atom of a molecule is in the
-    -- slice, then all atoms in the molecule will also be inside the slice 
-    resCloud=selectInSingleSlice(resCloud,true,sliceLowerLimits,sliceUpperLimits); 
-    -- Select edge atoms in rings 
-    resCloud=selectEdgeAtomsInRingsWithinSlice(rings,oCloud,resCloud,sliceLowerLimits,sliceUpperLimits,false);
+    -- slice, then all atoms in the molecule will also be inside the slice. We will also select edge atoms in rings
+    selectAtomsInSliceWithRingEdgeAtoms(outDir,rings,oCloud,resCloud,sliceLowerLimits,sliceUpperLimits,false);
 end
