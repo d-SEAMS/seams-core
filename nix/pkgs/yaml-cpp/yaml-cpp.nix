26577afbafc1739d552ff622f4d264666b6f5f40
Rohit Goswami <rgoswami@ieee.org>
2026-08-15 14:01:31 -0500

    chore: drop the CMake-era Nix yodaStruct derivation
    The live build is pixi. default.nix still named the product
    yodaStruct and used CMake. Front ends are their own repos.

--- nix/pkgs/yaml-cpp/yaml-cpp.nix
+++
@@ -1,9 +0,0 @@
-# [[file:~/Git/Github/C++/Forks/structureFactor/literateNix.org::*YAML%20Cpp][YAML Cpp:1]]
-# A standard cmake-based build
-{ clangStdenv, fetchJSON, cmake }:
-clangStdenv.mkDerivation {
-  name = "yaml-cpp-master";
-  src = fetchJSON ./yaml-cpp.src.json;
-  nativeBuildInputs = [ cmake ];
-}
-# YAML Cpp:1 ends here
