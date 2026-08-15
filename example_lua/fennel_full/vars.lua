093ee4ad06cd24b30e56131841f4cdd5d59c29d9
Rohit Goswami <rgoswami@ieee.org>
2026-08-15 06:29:36 -0500

    test(fennel): full-API parity example driving the registered surface

---
+++ example_lua/fennel_full/vars.lua
@@ -0,0 +1,9 @@
+print("Fennel full-API parity exercise")
+cutoffRadius = 3.5
+oxygenAtomType = 2
+targetFrame = 1
+maxDepth = 6
+mwTrajectory = "input/traj/mW_cubic.lammpstrj"
+mwAtomType = 1
+conFile = "input/con/tiny_multi_cuh2.con"
+functionScript = "example_lua/fennel_full/script.fnl"
